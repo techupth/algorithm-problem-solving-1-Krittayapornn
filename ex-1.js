057b5c34320c912cb1ca3208b00c9c56614c43b1
Krittayapornn <krittayaporn.ww@gmail.com>
2024-07-10 14:23:42 +0700

    problem solving ex1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+	function twoSum(numbers, target) {
+		let result = [];
+		for (let i = 0; i < numbers.length; i++) {
+		  for (let j = i+1; j <= numbers.length; j++) {
+			  console.log(j)
+			if (numbers[i] + numbers[j] === target) {
+			  result.push(i, j);
+			}
+		  }
+		}
+		return result
+	  }
+	  
+	  console.log(twoSum([2, 7, 11, 15], 9));
 };
